1facfaefbaa5fe6fcfcb9da1457e0998727d5da0
narwhaldisco <nward24@uwo.ca>
2020-06-03 21:35:15 -0400

    convert to web native!

---
+++ web/web.js
@@ -0,0 +1,40 @@
+let timer = 0
+
+function cycle() {
+  timer++
+
+  if (timer % 5 === 0) {
+    cpu.tick()
+    timer = 0
+  }
+
+  cpu.step()
+
+  setTimeout(cycle, 3)
+}
+
+
+async function loadRom() {
+    //const rom = event.target.value
+    const response = await fetch(`./roms/INVADERS`)
+    const arrayBuffer = await response.arrayBuffer()
+    const uint8View = new Uint8Array(arrayBuffer)
+    const romBuffer = new RomBuffer(uint8View)
+  
+    //console.log(romBuffer);
+
+    //cpu.interface.clearDisplay()
+    //cpu.interface.disableSound()
+    cpu.load(romBuffer)
+    //displayInstructions(rom)
+
+    cycle()
+}
+
+console.log("hello world")
+
+loadRom()
+
+console.log("loaded rom")
+
+//cycle()
